87eb2247a5540de48bd878be04193875dc11be1d
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-10-25 18:04:15 +0200

    BankiSzolgáltatás osztály kialakítása

---
+++ src/com/company/bankiszolgaltatasok/BankiSzolgaltatas.java
@@ -0,0 +1,9 @@
+package com.company.bankiszolgaltatasok;
+
+public class BankiSzolgaltatas {
+    private final Tulajdonos tulajdonos;
+
+    private BankiSzolgaltatas(Tulajdonos tulajdonos) {
+        this.tulajdonos = tulajdonos;
+    }
+}
